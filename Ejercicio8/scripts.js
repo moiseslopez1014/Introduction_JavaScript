cfc368dd48c08e60ee70ed47240cb19da206d2a9
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-13 21:51:54 +0200

    ex8

---
+++ Ejercicio8/scripts.js
@@ -0,0 +1,22 @@
+// ejercicio 8
+
+for (let secuenciaNumerica = 1; secuenciaNumerica<=15; secuenciaNumerica++) {
+    console.log(secuenciaNumerica);
+}
+
+//variante, cuenta atras del 10 al cero
+
+for (let secuenciaNumerica = 10; secuenciaNumerica >=0; secuenciaNumerica--) {
+    console.log(secuenciaNumerica);
+}
+
+//Variante extra, 123...
+
+for (let secuenciaNumerica = 1; secuenciaNumerica <=4; secuenciaNumerica++) {
+    if (secuenciaNumerica === 4) {
+        console.log(`pollito ingles`);
+    }
+    else {
+        console.log(secuenciaNumerica);
+    }
+}
