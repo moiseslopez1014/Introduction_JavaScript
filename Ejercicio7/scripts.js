b39bd88d1f8c544b9f501b7cc637a24dd3b237a7
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-13 21:34:06 +0200

    ex7

---
+++ Ejercicio7/scripts.js
@@ -0,0 +1,10 @@
+// ejercicio 7
+
+let secuenciaNumerica = 1;
+
+while (secuenciaNumerica<=30) {
+    if (secuenciaNumerica % 2 === 0) {
+        console.log(secuenciaNumerica);
+    }
+secuenciaNumerica++;
+}
