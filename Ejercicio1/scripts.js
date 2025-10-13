ba9181af1d388fbe013d6907c16d2f7a6470fbde
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-13 18:11:51 +0200

    ex1and2

---
+++ Ejercicio1/scripts.js
@@ -0,0 +1,14 @@
+// ejercicio 1
+
+let texto = "3";
+let numero = 5;
+let buleano = true;
+
+console.log(Number(texto)*numero+Number(buleano));
+
+//Variante
+
+let textillo = "1";
+let buleanillo = true;
+
+console.log(Number(textillo) + Number(buleanillo));
---
+++ Ejercicio2/scripts.js
@@ -0,0 +1,13 @@
+// ejercicio 2
+
+let side = 4;
+
+let area = side * side;
+
+console.log(area);
+
+// otro operador... al cuadrado?
+
+area = side ** 2;
+
+console.log(area);
