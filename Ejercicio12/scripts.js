e290d77f3caf8d541df321329059ec5e409a2d30
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-13 22:43:02 +0200

    ex12

---
+++ Ejercicio12/scripts.js
@@ -0,0 +1,9 @@
+// ejercicio 12
+
+let persona = {
+    nombre: 'Juan',
+    edad: 30,
+    trabajo: 'profesor'
+}
+
+console.log(`${persona.nombre} tiene ${persona.edad} años y trabaja de ${persona.trabajo}.`);
