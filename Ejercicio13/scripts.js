3f7935d6dc8b0c9cc466eb91db97059cc88d96a4
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-13 23:15:49 +0200

    ex13and14

---
+++ Ejercicio13/scripts.js
@@ -0,0 +1,7 @@
+// ejercicio 13
+
+let numeros = [12, 5, 8, 130, 44];
+
+let filtro = numeros.filter(num => num > 10);
+
+console.log(filtro);
---
+++ Ejercicio14/scripts.js
@@ -0,0 +1,7 @@
+// ejercicio 14
+
+let hora = 21;
+
+let respuesta = (hora >= 6 && hora <12) ? 'Buenos dias' : (hora >= 12 && hora <= 20) ? 'Buenas tardes' : 'Buenas noches';
+
+console.log(respuesta);
