39fd120d89deb6cf5bf19d959077b07ad1747d55
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-13 22:13:23 +0200

    ex10

---
+++ Ejercicio10/scripts.js
@@ -0,0 +1,13 @@
+// ejercicio 10 EXTRA
+
+function esPar(numero) {
+    if (numero % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(esPar(4));
+console.log(esPar(7));
