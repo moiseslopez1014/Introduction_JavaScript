b428ab626c53d760d0e9a998095c1465fac1d2b7
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-15 22:24:17 +0200

    ejercicios15done

--- Ejercicios 1410 practica/scripts.js
+++ Ejercicios 1410 practica/scripts.js
@@ -1,4 +1,4 @@
-
+/*
 //Ejercicios practica
 
 //1
@@ -87,7 +87,7 @@ const iWithY = items.findIndex(palabra => palabra.includes('y'));
 console.log('index with b:', iWithB);
 console.log('index with y:', iWithY);
 
-/*
+
 //Caso 1
 
 const nombre = 'Juan';
@@ -106,7 +106,7 @@ console.log('Edad:', edad, 'Ciudad:', ciudad);
 
 const paresEImpares = [1,2,3,4,5,6,7,8,9,10];
 paresEImpares.forEach(
-    paresEImpares % 2 === 0 ? console.log(paresEImpares + 'es par') : console.log(paresEImpares + 'es impar')
+    num => num % 2 === 0 ? console.log(num + 'es par') : console.log(num + 'es impar')
 )
 
 //caso 4
@@ -115,13 +115,15 @@ const activo = false;
 
 console.log('activo:', activo); //Es mas claro para debug porque el valor booleano sigue manteniendose sin transformarse en un string
 
+
 //ternarios
 
 const notas = [8,4,6,9,5,3];
 
-notas.forEach(
-    console.log('La nota ' + nota + (nota => nota < 6 ? 'reprobado' : 'aprobado'));
-)
+notas.forEach((nota) => { 
+  const estado = nota < 5 ? 'suspenso' : 'aprobado';
+  console.log('La nota ' + nota + ' es ' + estado);
+});
 //malll mu malll
 
 
@@ -130,8 +132,6 @@ const numeros = [10, -5, 0, 7, -3, 2];
 numeros.forEach(numero => {
     console.log('El numero ' + numero + (numero === 0 ? ' es cero' : numero < 0 ? ' es negativo' : ' es positivo'));
 })
-
-*/
 
 // Ejercicio 8
 
@@ -142,6 +142,7 @@ const dosPrimeras = letras.slice(0,2);
 
 const tresUltimas = letras.slice(-3);
     console.log(tresUltimas);
+/*
 
 // Ejercicio 9
 
@@ -157,11 +158,11 @@ console.log('Me sorprende que esto que parecia sencillo, de hecho lo fue. no se 
 
 // Ejercicio 10
 
-const numeros = [2,5,8,12];
+const numeros10 = [2,5,8,12];
 
-console.log('Alguno es mayor a 10?', numeros.some(num => num > 10));
-console.log('Todos son mayores que 0?', numeros.every(num => num > 0));
-console.log('El array contiene el 8?', numeros.includes(8));
+console.log('Alguno es mayor a 10?', numeros10.some(num => num > 10));
+console.log('Todos son mayores que 0?', numeros10.every(num => num > 0));
+console.log('El array contiene el 8?', numeros10.includes(8));
 
 //Ejercicio 11
 
@@ -211,3 +212,184 @@ desordenOrdenadoInvertido = [...desorden].sort((a,b) => b - a);
 console.log('Desorden:', desorden);
 console.log('Ordenamos:', desordenOrdenado);
 console.log('E invertimos:', desordenOrdenadoInvertido);
+
+
+//extra
+//split(): Rompe string en array
+
+let stringTest = 'moiseslopeziglesias';
+let arrayString = stringTest.split("");
+console.log(arrayString);
+
+
+// Ejercicios 15 octubre
+
+console.log("=== Ejercicios 15 Octubre ===");
+
+//1
+
+const matrizBi = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+console.log(matrizBi[0][0]);
+console.log(matrizBi[0][1]);
+console.log(matrizBi[0][2]);
+console.log(matrizBi[1][0]);
+console.log(matrizBi[1][1]);
+console.log(matrizBi[1][2]);
+console.log(matrizBi[2][0]);
+console.log(matrizBi[2][1]);
+console.log(matrizBi[2][2]);
+
+console.log("La segunda fila y tercera columna es: ", matrizBi[1][2]);
+
+//2
+
+for (let i = [0]; i < matrizBi.length; i++) {
+  for (let j = [0]; j < matrizBi[i].length; j++) {
+    console.log(matrizBi[i][j]);
+  }
+}
+
+let sumados = 0;
+for (let i = [0]; i < matrizBi.length; i++) {
+  for (let j = [0]; j < matrizBi[i].length; j++) {
+    sumados += matrizBi[i][j];
+  }
+}
+
+console.log("La suma es: ", sumados);
+
+//3
+
+const matrizTri = 
+[
+  [
+    [1,2],[3,4],
+  ],
+  [
+    [5,6],[7,8],
+  ],
+];
+
+console.log(matrizTri[0][0][0]);
+
+//4
+
+function cuentaAtras(i) {
+  if (i < 1) return;
+  console.log(i);
+  cuentaAtras(i-1);
+}
+
+cuentaAtras(5);
+*/
+//5
+
+function sumaRe(i) {
+  if (i <= 1) return 1;
+  return i + sumaRe(i-1);
+}
+
+let retornoSumaRe = sumaRe(3);
+console.log(retornoSumaRe);
+
+//6 
+
+function factorial(i) {
+  if (i <=1) return 1;
+  return i * factorial(i-1);
+}
+
+let retornoFactorial = factorial(5);
+console.log(retornoFactorial);
+
+//7
+
+const persona = {
+  nombre: 'Menganito',
+  edad: 33,
+  ciudad: 'Cabra'
+};
+
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.ciudad);
+
+persona.profesion = 'mamporrero';
+
+console.log(persona);
+
+persona.edad = 34;
+
+console.log(persona.edad);
+
+persona.saludar = function() { console.log('Hola', persona.nombre) }
+
+persona.saludar();
+
+//8
+
+const muchasPersonas = [
+  {
+    nombre: 'Juan',
+    edad: 23,
+    ciudad:'malaga'
+  },
+  {
+    nombre: 'danie',
+    edad: 30,
+    ciudad:'algeciras'
+  },
+  {
+    nombre: 'Dick',
+    edad: 20,
+    ciudad:'Breda'
+  }
+]
+
+console.log(muchasPersonas[0].nombre);
+console.log(muchasPersonas[1].nombre);
+console.log(muchasPersonas[2].nombre);
+
+//9
+
+const colores = ['rojo','verde','azul'];
+
+const [primerColor, segundoColor, tercerColor] = colores;
+
+console.log(segundoColor, tercerColor, primerColor);
+
+//10
+
+const numerosParaSeleccion = [23,12,33];
+
+const [numeroSeleccionado1, ,numeroSeleccionado2] = numerosParaSeleccion;
+console.log(numeroSeleccionado2,numeroSeleccionado1);
+
+//11
+
+const persona11 = {
+  nombre: 'Menganito',
+  edad: 33,
+  ciudad: 'Cabra',
+  profesion: 'programador',
+  hobbies: 'dormir'
+}
+
+const {nombre, edad} = persona11;
+
+console.log(nombre, edad);
+
+//12
+
+const {nombre: nombrePersona, edad: edadPersona} = persona11;
+
+console.log(nombrePersona, edadPersona);
+
+//REPASO
+
+console.log('====== REPASO ======');
